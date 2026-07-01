f87b8d76f8c62fba990dca01a9c300020b7c2389
Clément Gace <clementgace@outlook.com>
2026-07-01 11:24:08 +0200

    okay

--- src/app/nuit-service.ts
+++ src/app/nuit-service.ts
@@ -3,7 +3,7 @@ import { Injectable, Service } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class NuitService {
-    constructor(private http: HttpClient){};
+    constructor(private http: HttpClient){ };
 
     getMedecins(){
     return this.http.get('http://localhost:3000/medecins_validateurs');
--- src/app/resultats_nuits/resultats_nuits.ts
+++ src/app/resultats_nuits/resultats_nuits.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { NuitService } from '../nuit-service';
 import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-resultats-nuits',
-  imports: [FormsModule],
+  imports: [FormsModule,RouterOutlet,RouterLink],
   templateUrl: './resultats_nuits.html',
   styleUrl: './resultats_nuits.scss',
 })
@@ -27,14 +28,12 @@ export class ResultatsNuits {
 
   listerNuitsNonTraitees() {
     this.nuitService.getNuitsNonTraitees().subscribe({
-      next: (res: {NuitsNontraitees? : [{}]}) => {
-      
-        this.NuitsNontraitees.set(res["NuitsNontraitees"]);
-        console.log(this.NuitsNontraitees);
+      next: (res: {NuitsNontraitees? : []}) => {
+        this.NuitsNontraitees.set(res["NuitsNontraitees"]);;
       },
       error: err => console.error(err)
     });
   }
-
-
 }
+
+
