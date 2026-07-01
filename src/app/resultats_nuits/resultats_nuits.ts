0e490efc5cb24f05e626709ac6fa7ba79601e5e5
Malick FALL <emfall5015@gmail.com>
2026-07-01 15:02:08 +0200

    lier bouton et api

--- src/app/resultats_nuits/resultats_nuits.ts
+++ src/app/resultats_nuits/resultats_nuits.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { NuitService } from '../nuit-service';
 import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-resultats-nuits',
@@ -10,10 +11,12 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './resultats_nuits.scss',
 })
 export class ResultatsNuits {
+    result = signal<any>(null); // Signal qui accepte tout
 
-  constructor(private nuitService : NuitService) { };
+  constructor(private nuitService : NuitService, private http: HttpClient) { };
    commentaire: string = '';
-
+   inputIdNuit : number =0;
+   inputIdMedecin : number =0;
    ouvrirAppli(){
         window.open('http://localhost:8501/','_blank')
     }
@@ -21,7 +24,7 @@ export class ResultatsNuits {
   
 
   medecins = signal<any | null>(null);
-  
+   
 
   listerMedecins() {
     this.nuitService.getMedecins().subscribe({
@@ -44,6 +47,27 @@ export class ResultatsNuits {
     });
   }
 
+  lancerEtl(id_nuit: number, id_medecin: number,commentaire_medical:string) {
+    console.log("id_nuit",id_nuit);
+    console.log("id_medecin",id_medecin);
+    console.log("commentaire_medical",commentaire_medical);
+    return this.http.post('http://localhost:3000/lancer-etl-operateur',
+      {
+        "id_nuit": id_nuit,
+        "id_medecin": id_medecin,
+        "commentaire_medical" : commentaire_medical
+      }).subscribe({  //subscribe callback, envoie de la requete
+
+        next: result => {  //en cas de reussite 
+          this.result.set(result)
+        },
+        error: err => {
+          console.error(err);
+          this.result.set(null);
+        }
+      });
+  }
+  
 }
 
 
